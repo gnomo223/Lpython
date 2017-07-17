1b82c1ed2817ec155878e6f37ab5a5b30c8ac2a7
Vinicius <rodriguessv30@gmail.com>
2017-07-17 17:28:53 -0300

    Movido para testes

--- Exercícios/#3.py
+++
@@ -1,4 +0,0 @@
-dia = input('Dia = ')
-mes = input('mes = ')
-ano = input('Ano = ')
-print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. correto?')﻿
--- Exercícios/idade.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('Que ano estamos? '))
-n2 = int(input('Qual ano você nasceu? '))
-
-pause = input('Tecle enter para ver sua idade')
-
-resultado = n1 - n2
-
-
-print ('A sua idade é', resultado, 'anos!')

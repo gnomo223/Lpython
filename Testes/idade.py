738b9187b70c335075191fdaec92f34cdf741deb
Vinicius <rodriguessv30@gmail.com>
2017-07-17 17:32:22 -0300

    Adicionado

---
+++ Testes/idade.py
@@ -0,0 +1,9 @@
+n1 = int(input('Que ano estamos? '))
+n2 = int(input('Qual ano você nasceu? '))
+
+pause = input('Tecle enter para ver sua idade')
+
+resultado = n1 - n2
+
+
+print ('A sua idade é', resultado, 'anos!')

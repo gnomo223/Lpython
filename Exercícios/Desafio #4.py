7f1b92f780f33f882ff35f11be63bda7ea3a8afe
Vinicius <rodriguessv30@gmail.com>
2017-07-16 21:20:30 -0300

    :fire:

---
+++ Exercícios/Desafio #4.py
@@ -0,0 +1,8 @@
+﻿print('Script para msotrar informações dos valores digitados')
+valor = input('Digite algo: ')
+print('Informações obtidas de {}...'.format(valor))
+print('É alfanumérico: {} \nÉ alfabético: {} \nÉ decimal: {} \nÉ um dígito: {} \nÉ identificador: {} '
+      '\nÉ minúsculo: {} \nÉ número: {} \nÉ imprimível: {} \nÉ espaço: {} \nÉ título: {} \nÉ maiúsculo: {} '
+      '\nÉ do tipo: {}'.format(valor.isalnum(), valor.isalpha(), valor.isdecimal(), valor.isdigit(),
+      valor.isidentifier(), valor.islower(), valor.isnumeric(), valor.isprintable(),
+      valor.isspace(), valor.istitle(), valor.isupper(), type(valor)))
